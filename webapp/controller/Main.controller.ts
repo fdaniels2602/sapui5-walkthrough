1c93db6059ddf46f25646fe80e66e51a8ecdeadb
Fuad <fdaniels@pnp.co.za>
2024-06-13 17:46:00 +0200

    first commit

--- webapp/controller/Main.controller.ts
+++ webapp/controller/Main.controller.ts
@@ -1,11 +1,31 @@
 import MessageBox from "sap/m/MessageBox";
 import BaseController from "./BaseController";
+import JSONModel from "sap/ui/model/json/JSONModel";
+import ResourceBundle from "sap/base/i18n/ResourceBundle";
 
 /**
  * @namespace za.co.fuad.controller
  */
 export default class Main extends BaseController {
+
+	onInit(): void {
+		// set data model on view
+		const data = {
+			recipient: {
+				name: 'World'
+			}
+		};
+
+		const dataModel = new JSONModel(data);
+		this.getView()?.setModel(dataModel);
+
+	}
+
 	public sayHello(): void {
-		MessageBox.show("Hello World!");
+		// read message from the i18n model
+		const recipient = (this.getView().getModel() as JSONModel)?.getProperty("/recipient/name");
+		const resourceBundle = this.getResourceBundle() as ResourceBundle;
+		const msg = resourceBundle.getText("helloMsg", [recipient]) || "no text defined";
+		MessageBox.show(msg);
 	}
 }
