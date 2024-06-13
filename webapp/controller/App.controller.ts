dfcaff6b28bb5aabc8e725b02d6e5a5069cb1e6c
Fuad <fdaniels@pnp.co.za>
2024-06-13 16:36:04 +0200

    Initial commit

---
+++ webapp/controller/App.controller.ts
@@ -0,0 +1,11 @@
+import BaseController from "./BaseController";
+
+/**
+ * @namespace za.co.fuad.controller
+ */
+export default class App extends BaseController {
+	public onInit(): void {
+		// apply content density mode to root view
+		this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+	}
+}
