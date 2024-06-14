e50e4db345b8f82e734071c4b80bc5a49097394a
Fuad <fdaniels@pnp.co.za>
2024-06-14 16:49:54 +0200

    After step 26 - mock server

--- webapp/controller/InvoiceList.controller.ts
+++ webapp/controller/InvoiceList.controller.ts
@@ -1,6 +1,9 @@
 import BaseController from "./BaseController";
 import JSONModel from "sap/ui/model/json/JSONModel";
-
+import { SearchField$SearchEvent } from "sap/m/SearchField";
+import Filter from "sap/ui/model/Filter";
+import FilterOperator from "sap/ui/model/FilterOperator";
+import ListBinding from "sap/ui/model/ListBinding";
 import formatter from "../model/formatter";
 
 /**
@@ -17,4 +20,20 @@ export default class App extends BaseController {
         });
         this.getView()?.setModel(viewModel, "view");        
     } 
+
+    onFilterInvoices(evt: SearchField$SearchEvent): void {
+        // build filter array
+        const filter = [];
+        const query = evt.getParameter("query");
+
+        // if query is captured then add as a filter
+        if (query) {
+            filter.push(new Filter("ProductName", FilterOperator.Contains, query));
+        }
+
+        // filter binding
+        const list = this.byId("InvoiceList");
+        const binding = list?.getBinding("items") as ListBinding;
+        binding ?.filter(filter);
+    }
 }
