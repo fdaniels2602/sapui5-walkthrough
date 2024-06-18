9e23f0964064c90a4c01999a01dcbe6ca745731d
fdaniels2602 <fuad2602@gmail.com>
2024-06-18 19:50:33 +0200

    All steps completed

--- webapp/controller/Detail.controller.ts
+++ webapp/controller/Detail.controller.ts
@@ -1,9 +1,15 @@
 import { Route$PatternMatchedEvent } from "sap/ui/core/routing/Route";
 import BaseController from "./BaseController";
+import JSONModel from "sap/ui/model/json/JSONModel";
 
 export default class Detail extends BaseController {
 
     onInit(): void {
+        const viewModel = new JSONModel({
+            currency: "EUR"
+        });
+        this.getView().setModel(viewModel, "view");
+        
         const router = this.getOwnerComponent().getRouter();
         router.getRoute("detail").attachPatternMatched(this.onObjectMatched, this);
 
